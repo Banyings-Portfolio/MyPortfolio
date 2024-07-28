efe13eb4fe1bc1f4cc815ef0e2814d8263e8d921
Hyppolite Banyingela <banyingelahyppo@gmail.com>
2024-07-27 23:52:54 -0600

    education cleaning

--- my-portfolio/src/app/about/EducationWidget/Education.tsx
+++ my-portfolio/src/app/about/EducationWidget/Education.tsx
@@ -3,13 +3,12 @@ export default function Education() {
         <div className="pt-4 pl-8 w-auto h-auto border-2 border-gray-300 rounded-lg p-6 shadow-md mx-4 my-4">
             <h1 className="font-bold text-2xl pb-6">Education</h1>
             <div>
-                <p className="pb-2">I am running for an associate degree of science and a certificate in Software Engineering. <br/>
-                I was not sure if it would have been a suitable field for me at first because I was so scared of programming. <br/>
-                After giving it a try, I found out that I can do it
+                <p className="pb-2">
+                    I am pursuing an Associate of Science degree and a certificate in Software Engineering. At first, I was unsure if it would be a suitable field for me because I was intimidated by programming. However, after giving it a try, I discovered that I can indeed do it.
                 </p>
             </div>
             <div className="pt-4">
-                <img src="/ensign.png" alt="Ensign college logo" className="w-20 h-20"/>
+                <img src="/ensign.png" alt="Ensign college logo" className="w-20 h-20" />
             </div>
             <div className="pt-4">
                 <div className="flex justify-between items-center">
